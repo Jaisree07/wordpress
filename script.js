e08aec0e00c7654a19fa016650749cbf6c22aa0a
jaisree <jaisree.anandhakumar@psiog.com>
2025-09-28 13:11:32 +0530

    fonttype

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ formatButtons.forEach(button => {
     document.execCommand(command, false, null); 
   });
 });
+
+const fontselect = document.getElementById('font');
+fontselect.addEventListener('change', () => {
+  const font = fontselect.value;
+  document.execCommand('fontName', false, font);
+  editor.focus();
+});
+
+const sizeselect = document.getElementById('size');
+const sizeMap = {"12": 2,"14": 3,"16": 4,"18": 5,"22": 6,"24": 7};
+sizeselect.addEventListener('change', () => {
+  const px = sizeselect.value;
+  const size = sizeMap[px];
+  document.execCommand('fontSize', false, size);
+  editor.focus();
+});
